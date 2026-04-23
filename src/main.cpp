b256e06c5f0d70a8504163fec9adc1a21cce2984
EthanRossmath <ethanross981@gmail.com>
2026-04-23 12:20:32 -0400

    making the project less python-y

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,42 @@
 #include "mandelbrot.h"
 #include <filesystem>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+
 
 //This is an implementation using the future-based mandelbrot set generator.
 //The syntax for the other algorithms is essentially the same.
 
-int main()
+const std::filesystem::path outDir = "images";
+const std::string defaultFileName = "test.bmp";
+
+void write_to_file(MandelbrotImage& mand_im, std::string file_name)
+{
+    std::filesystem::path outPath = outDir / file_name;
+
+    mand_im.writeBMP(outPath.string());
+
+}
+
+bool is_numeric(const std::string& str)
+{
+    if (str.empty())
+    {
+        return false;
+    }
+
+    std::string::const_iterator it = std::find_if(str.begin(), str.end(), [](char c){
+        return !std::isdigit(c);
+    });
+
+    return it == str.end();
+}
+
+
+
+int main(int argc, char* argv[])
 {
     // To create a Mandelbrot set, you need to first specify the bounds of the 
     // horizontal axis and the vertical axis.
@@ -22,25 +54,96 @@ int main()
         verticalUpperLimit
     };
 
-    // Below is the future-based implementation of the Mandelbrot-set generator
-    const int numFutures{8}; // my machine has 8 cores, hence I chose 8
-    mi.genFractalFuture(numFutures); // the number of futures must be specified
-    // when generating your fractal
+    // If no arguments are given, default to doing slowest generation 
+    // algorithm
+    if (argc == 1)
+    {
+        mi.genFractal();
+        write_to_file(mi, defaultFileName);
+        return 0;
+    }
 
-    // The more primitive threads-based algorithm should be invoked via
-    // mi.genFractalThreads(numThreads) where you must specify the number of threads
-    // My machine has 8 cpu cores, so I would choose numThreads = 8.
+    // Find file name
+    std::string fileName{};
+    for (int i{1}; i < argc; ++i)
+    {
+        if (std::strstr(argv[i], ".bmp"))
+        {
+            fileName = std::string(argv[i]);
+            break;
+        }
+    }
 
-    // The OpenMP-based implementation follows a similar syntax 
-    // mi.genFractalOpenMP()
+    if (fileName.length() == 0)
+    {
+        fileName = defaultFileName;
+    }
 
-    // Try using the other algorithms to compare the speed of these approaches.
+    // Find algorithm type.
+    // Key: 'd' == default (no parallelization)
+    //      'o' == OpenMP
+    //      'f' == futures
+    //      't' == threads
+    char method{'d'};
+    for (int i{1}; i < argc; i++)
+    {
 
-    // The directory where the .bmp file will be saved is specified below
-    const std::filesystem::path outDir = "images";
+        if (std::strstr(argv[i], "open"))
+        {
+            method = 'o';
+            break;
+        }
+        else if (std::strstr(argv[i], "future"))
+        {
+            method = 'f';
+            break;
+        }
+        else if (std::strstr(argv[i], "thread"))
+        {
+            method = 't';
+            break;
+        }
+    }
 
-    std::filesystem::path outPath = outDir / "test.bmp";
+    if (method == 'd')
+    {
+        mi.genFractal();
+        write_to_file(mi, fileName);
+        return 0;
+    }
 
-    mi.writeBMP(outPath.string());
+    if (method == 'o')
+    {
+        mi.genFractalOpenMP();
+        write_to_file(mi, fileName);
+        return 0;
+    }
+
+    // Get number of threads, futures, or cores for threads, futures, OpenMP,
+    // respectively.
+
+    int var{1};
+    for (int i{1}; i < argc; ++i)
+    {
+        if (is_numeric(std::string(argv[i])))
+        {
+            var = std::stoi(argv[i]);
+        }
+    }
+
+    if (method == 'f')
+    {
+        mi.genFractalFuture(var);
+        write_to_file(mi, fileName);
+        return 0;
+    }
+
+    if (method == 't')
+    {
+        mi.genFractalThread(var);
+        write_to_file(mi, fileName);
+        return 0;
+    }
+    
     return 0;
 }
